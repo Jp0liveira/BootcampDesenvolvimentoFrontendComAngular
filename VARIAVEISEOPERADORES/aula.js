4f47898868c675f6afa8126cb3f20320826c73e6
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-01-02 09:49:25 -0300

    feat: exercicio sobre variaveis e operadores

---
+++ VARIAVEISEOPERADORES/aula.js
@@ -0,0 +1,9 @@
+const precoCombustivel  = 5.79;
+const gastoMedioCombustivelKM = precoCombustivel / 10;
+
+let distanciaVagemKM = 100;
+
+let valorGastoViagem =  distanciaVagemKM * gastoMedioCombustivelKM;
+
+console.log(valorGastoViagem);
+
